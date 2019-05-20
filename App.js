ca243b05b7356d2998b881e0cadfebe69142a481
Ham Choromi <choromi0804@gmail.com>
2019-05-20 21:16:38 +0900

    Add complete and uncomplete logic (2nd)

--- App.js
+++ App.js
@@ -113,6 +113,20 @@ export default class App extends React.Component {
       };
       return { ...newState };
     });
+    _completeToDo = id => {
+      this.setState(prevState => {
+        const newState = {
+          ...prevState,
+          toDos: {
+            ...prevState.toDos,
+            [id]: {
+              ...prevState.toDos[id],
+              isCompleted: true
+            }
+          }
+        };
+        return { ...newState };
+    }
   };
 }
 
