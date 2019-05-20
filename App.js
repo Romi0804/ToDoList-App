90be5f321b417b835aa63bab9579cc4e0038993c
Ham Choromi <choromi0804@gmail.com>
2019-05-20 21:55:58 +0900

    Add ToDo Component's new function

--- App.js
+++ App.js
@@ -46,7 +46,13 @@ export default class App extends React.Component {
           />
           <ScrollView contentContainerStyle={styles.toDos}>
             {Object.values(toDos).map(toDo => (
-              <ToDo key={toDo.id} {...toDo} deleteToDo={this._deleteToDo} />
+              <ToDo
+                key={toDo.id}
+                deleteToDo={this._deleteToDo}
+                uncompleteToDo={this._uncompleteToDo}
+                completeToDo={this._completeToDo}
+                {...toDo}
+              />
             ))}
           </ScrollView>
         </View>
